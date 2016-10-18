20199250c9fac7c92bc3ba3672602fb2d1651955
Yuri Artemev <i@artemeff.com>
2016-10-18 12:24:48 +0300

    add coverage tool and package meta info, update tl

--- mix.exs
+++ mix.exs
@@ -7,28 +7,40 @@ defmodule MTProto.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: preferred_cli_env,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:crypto, :connection, :tl]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp preferred_cli_env do
+    ["coveralls": :test,
+     "coveralls.detail": :test,
+     "coveralls.post": :test,
+     "coveralls.html": :test]
+  end
+
   defp deps do
     [{:connection, "~> 1.0.4"},
-     {:tl, "~> 57.0.0-beta"},
-     {:mock, "~> 0.2.0", only: :test}]
+     {:tl, "~> 57.1.0-rc"},
+     {:mock, "~> 0.2.0", only: :test},
+     {:excoveralls, "~> 0.5.7", only: :test},
+     {:ex_doc, "~> 0.14", only: :dev},
+     {:inch_ex, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    "MTProto transport for Elixir"
+  end
+
+  defp package do
+    [maintainers: ["Yuri Artemev", "Alexander Malaev"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/ccsteam/mtproto"}]
   end
 end
