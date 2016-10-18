f9c3c0371dceedac096231c039cf67d475f4c2eb
Yuri Artemev <i@artemeff.com>
2016-10-18 12:25:02 +0300

    bump version to 57.0.0-alpha

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MTProto.Mixfile do
 
   def project do
     [app: :mtproto,
-     version: "0.1.0",
+     version: "57.0.0-alpha",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
